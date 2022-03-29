b7db985cbc686cbdbe91f14cad90dd9fbcd9689c
Azad <azad.baykara96@gmail.com>
2022-03-29 18:44:03 +0300

    initial version of frontend

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <button type="submit">List</button>
+    <button type="submit">Add New Button</button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+  },
+};
+</script>
