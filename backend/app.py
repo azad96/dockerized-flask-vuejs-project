c56f7ff3a1d7e0d1f5e2a002ccd092bb022bbc1c
Azad <azad.baykara96@gmail.com>
2022-03-31 17:56:09 +0300

    post method is implemented

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from config import user, password
 from models import db, Person, Plate, PersonPlate
 import json
+import re
 
 app = Flask(__name__)
 
@@ -32,11 +33,31 @@ def plate():
 
     if request.method == 'POST':
         req = json.loads(list(dict(request.form).keys())[0])
+        plate_pattern = re.compile(r"[a-zA-Z]{1,3}-[a-zA-Z]{1,2}[1-9][0-9]{0,3}")
         print(req['plate_number'])
         print(req['owner_name'])
         print(req['start_date'])
         print(req['end_date'])
-        return {'response': 'success'}
+        if 'plate_number' not in req:
+            print('plate number is not in request')
+            return {'response': 400}
+        elif not plate_pattern.match(req['plate_number']):
+            print('plate number is not valid')
+            return {'response': 422}
+        else:
+            print('plate number is valid')
+            new_person = Person(req['owner_name'])
+            new_plate = Plate(req['plate_number'], req['start_date'], req['end_date'])
+            db.session.add(new_person)
+            db.session.add(new_plate)
+            db.session.commit()
+            new_person_plate_mapping = PersonPlate(new_person.id, new_plate.id)
+            db.session.add(new_person_plate_mapping)
+            db.session.commit()
+            return {'response': 200}
+
+
+
 
 
 # Start the app
